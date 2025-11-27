39fe49b988ad2e317049d05f36571889038764b9
ferran.martinezi.7e9 <ferran.martinezi.7e9@itb.cat>
2025-11-27 17:22:50 +0100

    Nous exercicis!

--- src/Activitat 1.15.kt
+++ src/Activitat 1.15.kt
@@ -7,4 +7,20 @@ fun main(){
     //Demanem els segons a l'usuari
     println("Indiqui un numero en segons :")
     var segons : Int = scanner.nextInt()
+
+    //Declarem un contador de temps
+    var temps : Int = 0
+
+    //Creem un bucle for a on sumem 1 al contador.
+    for (i in 0..segons){
+        temps++
+    }
+
+    //Si el contador ha arribat a 60 el tornem a posar a 0
+    if (temps == 60){
+        temps = 0
+        println(temps)
+        
+    //Sino imprimim el temps
+    }else println(temps)
 }
---
+++ src/Ativitat 1.16.kt
@@ -0,0 +1,12 @@
+//Program: Transforma l'enter
+import java.util.Scanner
+
+fun main(){
+    val scan : Scanner = Scanner(System.`in`)
+
+    //Demanem la variable.
+    var numeroEnter : Int = scan.nextInt()
+
+    //Imprimim el resultat per pantalla.
+    println(numeroEnter.toFloat())
+}
