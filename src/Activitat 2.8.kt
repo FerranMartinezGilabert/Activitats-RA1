c07a2c1df48630cbc42f00c60949edad846b32be
FerranMartinezGilabert <ferran.martinezi.7e9@itb.cat>
2026-01-12 19:47:15 +0100

    Activitat 2.8

--- src/Activitat 2.8.kt
+++ src/Activitat 2.8.kt
@@ -1,4 +1,3 @@
-import java.text.SimpleDateFormat
 import java.util.Scanner
 
 //Program : Afegeix un segon(2)
@@ -9,20 +8,27 @@ fun main(){
     val scan  = Scanner(System.`in`)
 
     //Demanem hora, minut i segons al usuari.
-    val HORA : Int = scan.nextInt()
-    val MINUT : Int = scan.nextInt()
-    val SEGON : Int = scan.nextInt()
-    var tempsHora : Int = 0
-    var tempsMin : Int = 0
-    var tempsSec : Int = 0
-    var dateFormat = SimpleDateFormat("hh:mm:ss")
-    //Sumem un segon.
-    if (SEGON + 1 > 60){
-        tempsSec += SEGON + 1
-        tempsMin += MINUT
-        tempsHora += HORA
+    var hora : Int = scan.nextInt()
+    var minut : Int = scan.nextInt()
+    var segon : Int = scan.nextInt()
+    var tempsHora : Int = hora
+    var tempsMin : Int = minut
 
-        //Imprimim resultat per pantalla.
-        println(tempsHora.)
-    }else println()
+    //Sumem 1 segon.
+    var tempsSec : Int = segon + 1
+
+    //Comprobem si els segons, minuts i hores arriben a 60 i 24 respectivament.
+    if (tempsSec >= 60){
+        tempsSec = 0
+        tempsMin += 1
+    }
+    if (tempsMin >= 60){
+        tempsMin = 0
+        tempsHora += 1
+    }
+    if (tempsHora >= 24){
+        tempsHora = 0
+    }
+    //Imprimim el resultat per pantalla, %02d transforma el format a dos dígits per exemple 7 a 07. Font: El amigo.
+    println("%02d:%02d:%02d".format(tempsHora, tempsMin, tempsSec))
 }
